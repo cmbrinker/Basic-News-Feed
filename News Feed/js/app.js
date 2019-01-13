e84c842f0ed477260e36ac2affb368c01d4cb0ca
Chris Brinker <cmbrinker@gmail.com>
2019-01-13 18:47:08 -0500

    add class secure on checkbox toggle

--- News Feed/js/app.js
+++ News Feed/js/app.js
@@ -2,13 +2,20 @@ const $odd= $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
 const $checkbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
+const checked = 0;
 
 $secureLinks.attr('target', '_blank');
 
 $secureLinks.addClass('secure');
-$pdfs.addClass('pdf');
 
 $('#links').append($checkbox);
+
+//Have to set a value that can toggle between classes with toggleClass()//
+
+$('input:checkbox').change(function() {
+  if($(this).is(':checked'))
+    $pdfs.addClass('secure');
+});
 
 $pdfs.on('click', function(event) {
   if ($(':checked').length === 0) {
